3910b42be206dba5722435792cf037a6a6325111
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-10-29 22:33:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,6 @@ node generate-keys.js service myservice
 node generate-keys.js nex myservice
 node generate-keys.js account myservice
 
+php -r 'echo json_encode(require("config.php"), JSON_PRETTY_PRINT);' > config.json
+
 npm run start
