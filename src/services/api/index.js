a750cffd86d5010811e7bc65dce887c5509e3a11
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-01 19:42:05 +0100

    Update index.js

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const APIMiddleware = require('../../middleware/api');
 const logger = require('../../logger');
 
+const CONNECTIONS = require('./routes/connections');
+const EMAIL = require('./routes/email');
+const FORGOT_PASSWORD = require('./routes/forgotPassword');
+const LOGIN = require('./routes/login');
+const REGISTER = require('./routes/register');
+const RESET_PASSWORD = require('./routes/resetPassword');
+const USER = require('./routes/user');
+
 const api = express.Router();
 
 logger.info('[API] Importing middleware');
@@ -13,24 +21,14 @@ api.use(APIMiddleware);
 api.use(cors());
 api.options('*', cors());
 
-const V1 = {
-	CONNECTIONS: require('./routes/connections'),
-	EMAIL: require('./routes/email'),
-	FORGOT_PASSWORD: require('./routes/forgotPassword'),
-	LOGIN: require('./routes/login'),
-	REGISTER: require('./routes/register'),
-	RESET_PASSWORD: require('./routes/resetPassword'),
-	USER: require('./routes/user'),
-};
-
 logger.info('[API] Applying imported routes');
-api.use('/v1/connections', V1.CONNECTIONS);
-api.use('/v1/email', V1.EMAIL);
-api.use('/v1/forgot-password', V1.FORGOT_PASSWORD);
-api.use('/v1/login', V1.LOGIN);
-api.use('/v1/register', V1.REGISTER);
-api.use('/v1/reset-password', V1.RESET_PASSWORD);
-api.use('/v1/user', V1.USER);
+api.use('/v1/connections', CONNECTIONS);
+api.use('/v1/email', EMAIL);
+api.use('/v1/forgot-password', FORGOT_PASSWORD);
+api.use('/v1/login', LOGIN);
+api.use('/v1/register', REGISTER);
+api.use('/v1/reset-password', RESET_PASSWORD);
+api.use('/v1/user', USER);
 
 const router = express.Router();
 
